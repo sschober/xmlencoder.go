721f7efc2e5cea26a442f3a55403e02ba4d16567
Sven Schober <sschober@sssm.de>
2010-07-14 07:24:59 +0200

    less verbosity, more accurate

--- encode_test.go
+++ encode_test.go
@@ -14,11 +14,9 @@ type Roar struct {
 func TestMarshal(t *testing.T) {
     var r = &Roar{Author: "Sven", Text: "Hallo, das ist ein Roar!",
         CreationDate: "09.07.2010 09:44"}
-    fmt.Printf("Trying to marshal...")
     result, err := Marshal(r)
-    fmt.Printf("done.\n")
     if err != nil {
-        t.Fatalf("Marshal allValue: %v", err)
+        t.Fatalf("Marshal r: %v", err)
     }
     fmt.Printf("%s", string(result))
 }
